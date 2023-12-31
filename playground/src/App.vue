ba81c1715d86cbb76b8569e9d85e2d83a1c405e1
fuzhiqiang <fuzhiqiang@baidu.com>
2023-12-31 23:12:19 +0800

    feat: npm

---
+++ playground/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NetworkSDK />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NetworkSDK from "network-speed-js";
+console.log(NetworkSDK, 'NetworkSDK')
+
+
+</script>
+
+<style scoped></style>
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as NetworkSDK } from "./App.vue";
